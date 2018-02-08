a2405ac7c72899374e239aa6a9aee49ac483b5a7
mdouchement <marc.douchement@gmail.com>
2018-02-08 11:26:25 +0100

    Fix UI glitches

--- public/assets/javascripts/app.js
+++ public/assets/javascripts/app.js
@@ -15,7 +15,8 @@ let app = new Vue({
     filter: '',
     categories: [],
     activeTab: 0,
-    items: {}
+    items: [],
+    items_map: {}
   },
   methods: {
     appendCategory: function(category) {
@@ -24,12 +25,12 @@ let app = new Vue({
       }
     },
     removeCategory: function(category) {
-      if (this.items[category].length === 0) {
-        i = _.findIndex(this.categories, category)
-        this.categories.splice(i, 1)
-        // FIXME generates glitches in UI
-        this.activeTab = 0
-      }
+      // if (this.items_map[category].length === 0) {
+      //   i = _.findIndex(this.categories, category)
+      //   this.categories.splice(i, 1)
+      //   // FIXME generates glitches in UI
+      //   this.activeTab = 0
+      // }
     },
     clearFilter: function() {
       this.filter = ''
@@ -41,24 +42,37 @@ let app = new Vue({
     closeNewItem: function() {
       this.inNewItem = false
     },
-    appendItem: function(item, autoSwitchTab=true) {
+    appendItem: function(item, pos=-1, autoSwitchTab=true) {
       this.appendCategory(item.category)
-      if (this.items[item.category] === undefined) {
-        this.$set(this.items, item.category, [])
+      if (this.items_map[item.category] === undefined) {
+        this.$set(this.items_map, item.category, [])
       }
-      this.items[item.category].push(item)
+      if (pos == -1) {
+        // New item
+        this.items_map[item.category].push(this.items.length)
+        this.items.push(item)
+      } else {
+        // Already added item
+        this.items_map[item.category].push(pos)
+      }
 
       if (autoSwitchTab) {
         let self = this
         _.delay(item => {
           // FIXME generates glitches in UI on new fresh category
-          self.activeTab = _.sortedIndex(self.sortedCategories, item.category) // Auto-switch tab
+          self.activeTab = _.indexOf(self.categories, item.category) // Auto-switch tab
         }, 200, item)
       }
     },
     removeItem: function(item) {
-      let i = _.findIndex(this.items, item)
-      this.items[item.category].splice(i, 1)
+      let pos = _.findIndex(this.items, item)
+      let i = _.findIndex(this.items_map[item.category], pos)
+      this.items_map[item.category].splice(i, 1)
+      // Sync mapping
+      _.each(this.categories, c => {
+        this.items_map[c] = _.map(this.items_map[c], cpos => cpos > pos ? cpos-1 : cpos)
+      })
+      this.items.splice(pos, 1)
       this.removeCategory(item.category)
     },
     getItems: function() {
@@ -70,7 +84,13 @@ let app = new Vue({
         }
       })
       .then(function(response) {
-        _.each(response.data, i => self.appendItem(i, false))
+        self.items = _.orderBy(response.data, ['score', 'name'], ['desc', 'asc'])
+        let pos = 0
+        _.each(self.items, i => {
+          self.appendItem(i, pos, false)
+          pos++
+        })
+        self.categories = self.categories.sort()
       })
       .catch(function(error) {
         console.log(error)
@@ -79,16 +99,13 @@ let app = new Vue({
     }
   },
   computed: {
-    sortedCategories: function() {
-      return this.categories.sort()
-    },
-    sortedItems: function() {
-      return _.orderBy(this.items[this.sortedCategories[this.activeTab]], ['score', 'name'], ['desc', 'asc'])
+    itemPool: function() {
+      return _.map(this.items_map[this.categories[this.activeTab]], pos => this.items[pos])
     },
     filteredItems: function() {
       bus.$emit('item-selected', 'collapse-all') // Force collapse all on search
       let filter = _.toLower(this.filter)
-      return _.filter(this.sortedItems, i => _.startsWith(_.toLower(i.name), filter))
+      return _.filter(this.itemPool, i => _.startsWith(_.toLower(i.name), filter))
     }
   }
 })
